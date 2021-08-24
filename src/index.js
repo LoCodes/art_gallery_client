85d3ed237bd7696787f30fb2fb3d8da71ff3db5a
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-24 12:03:55 -0700

    set up imports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// react-redux, redux, thunk 
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+// create a store for redux => createStore 
