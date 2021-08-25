43a9db108554d829b65a1404713b7b254b2fafc9
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-25 13:12:57 -0700

    create home page component and add in router.js

---
+++ src/components/Router.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Switch, Router } from 'react-router-dom'
+import Home from './Home'
+
+const Router = () => {
+    
+    return (
+        <div>
+                
+        </div>
+    )
+}
+
+export default Router
