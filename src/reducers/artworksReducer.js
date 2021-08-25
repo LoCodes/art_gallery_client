ad7c35eacb635d4eb88b2a81623d4fc9ad3c5d4c
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-24 17:25:54 -0700

    added case in artworksreducer for adding artwork

--- src/reducers/artworksReducer.js
+++ src/reducers/artworksReducer.js
@@ -2,8 +2,10 @@ export const artworksReducer = ( state = [], action ) => {
     switch(action.type){
         case 'FETCH_ARTWORKS': 
             return action.payload
+        case 'ADD_ARTWORK':
+            return [...state, action.payload]
         default:
-            return state 
+            return state
     }
 
 }
